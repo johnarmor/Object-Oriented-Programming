ffddd9b37ce3031b2bdaf3db494fb88b2c1dfcc0
johnarmor <100022397+johnarmor@users.noreply.github.com>
2022-10-15 19:52:40 +0800

    Create Main.java

---
+++ 3.3.5.2.1 Learning Task 3/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        College3 c3 = new College3();
+
+        c3.course();
+        c3.subject();
+        c3.teacher();
+        c3.students();
+        c3.schedule();
+
+    }
+
+}
